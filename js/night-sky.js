17443e9b6761adb4e26e302d45a57456a3782fd9
Conner Chappell <conner.chappell123@gmail.com>
2022-02-24 13:51:09 -0700

    Transferring files and set up from test2

--- js/night-sky.js
+++ js/night-sky.js
@@ -1,5 +1,10 @@
 const modal = document.querySelector('#welcome-modal')
 const closeModalBtn = document.querySelector('#close-modal-btn')
+// light tab selectors
+const lightTab1 = document.querySelector('#light-tab1')
+const lightTab2 = document.querySelector('#light-tab2')
+const lightTab3 = document.querySelector('#light-tab3')
+const lightTab4 = document.querySelector('#light-tab4')
 
 // click event listener that closes welcome modal
 closeModalBtn.addEventListener('click', () => {
