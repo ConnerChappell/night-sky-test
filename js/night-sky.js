830c026bdb8c67f4a76959a56e3c6773297a0e26
Conner Chappell <conner.chappell123@gmail.com>
2022-02-26 15:15:25 -0700

    night sky 2 displays when light tab 1 is clicked

--- js/night-sky.js
+++ js/night-sky.js
@@ -1,3 +1,4 @@
+const body = document.querySelector('body')
 const modal = document.querySelector('#welcome-modal')
 const closeModalBtn = document.querySelector('#close-modal-btn')
 // light tab selectors
@@ -5,6 +6,14 @@ const lightTab1 = document.querySelector('#light-tab1')
 const lightTab2 = document.querySelector('#light-tab2')
 const lightTab3 = document.querySelector('#light-tab3')
 const lightTab4 = document.querySelector('#light-tab4')
+
+
+///////////////////////// event listeners
+
+// click event to display night sky 2
+lightTab1.addEventListener('click', () => {
+    body.classList.add('night-sky2')
+})
 
 // click event listener that closes welcome modal
 closeModalBtn.addEventListener('click', () => {
